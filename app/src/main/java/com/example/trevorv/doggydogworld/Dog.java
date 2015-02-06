34b44a50536efa788dae04d64d105119cc554f6d
Trevor Voth <TrevorV@MP-TrevorV.local>
2015-02-06 15:52:59 -0800

    First Commit

---
+++ app/src/main/java/com/example/trevorv/doggydogworld/Dog.java
@@ -0,0 +1,31 @@
+package com.example.trevorv.doggydogworld;
+
+import android.util.Log;
+
+/**
+ * Created by TrevorV on 2/6/15.
+ */
+public class Dog {
+
+    private String name;
+    private int age;
+    private int numberOfLegs = 4;
+    private String breed;
+
+    Dog(){
+        Log.d("Dog", "Constructing...");
+    }
+    Dog(String dogName){
+        name = dogName;
+    }
+
+    public void bark(){
+        String bark = name + " barked";
+        Log.d("Dog", bark);
+    }
+
+    public void barkat(String victim){
+        String bark = name + " barked at "+victim;
+        Log.d("Dog", bark);
+    }
+}
