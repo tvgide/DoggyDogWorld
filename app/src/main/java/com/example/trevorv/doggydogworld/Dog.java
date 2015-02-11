bc74c42b7584f3a6e61c06a555dd572167c03d68
tvgide <trevor.voth@gmail.com>
2015-02-11 10:18:36 -0800

    021120151017

--- app/src/main/java/com/example/trevorv/doggydogworld/Dog.java
+++ app/src/main/java/com/example/trevorv/doggydogworld/Dog.java
@@ -12,20 +12,21 @@ public class Dog {
     private int numberOfLegs = 4;
     private String breed;
 
-    Dog(){
+    Dog() {
         Log.d("Dog", "Constructing...");
     }
-    Dog(String dogName){
+
+    Dog(String dogName) {
         name = dogName;
     }
 
-    public void bark(){
+    public void bark() {
         String bark = name + " barked";
         Log.d("Dog", bark);
     }
 
-    public void barkat(String victim){
-        String bark = name + " barked at "+victim;
+    public void barkat(String victim) {
+        String bark = name + " barked at " + victim;
         Log.d("Dog", bark);
     }
 }
